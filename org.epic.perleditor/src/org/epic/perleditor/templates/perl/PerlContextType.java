2b828853467bbc67057202a445cdc68e83890921
luelljoc <>
2003-10-04 11:39:33 +0000

    Dollar ($$) variable added.

--- org.epic.perleditor/src/org/epic/perleditor/templates/perl/PerlContextType.java
+++ org.epic.perleditor/src/org/epic/perleditor/templates/perl/PerlContextType.java
@@ -76,7 +76,7 @@ public class PerlContextType extends CompilationUnitContextType {
 		
 		// global
 		addVariable(new GlobalVariables.Cursor());
-		//addVariable(new GlobalVariables.Dollar());
+		addVariable(new GlobalVariables.Dollar());
 		addVariable(new GlobalVariables.Date());
     	addVariable(new GlobalVariables.Year());
 		addVariable(new GlobalVariables.Time());

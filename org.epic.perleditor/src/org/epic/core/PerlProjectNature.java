53c775047b0a62ba6ce9b8e2a4e9d65dcd22b682
luelljoc <>
2004-01-05 07:50:40 +0000

    Builder added

--- org.epic.perleditor/src/org/epic/core/PerlProjectNature.java
+++ org.epic.perleditor/src/org/epic/core/PerlProjectNature.java
@@ -20,8 +20,8 @@ import org.eclipse.core.runtime.CoreException;
  */
 public class PerlProjectNature implements IProjectNature {
 
-private IProject project;
-private String PLUGIN_ID = "org.epic.perleditor";
+	private IProject project;
+	private String PLUGIN_ID = "org.epic.perleditor";
 	/**
 	 * 
 	 */
@@ -35,10 +35,35 @@ private String PLUGIN_ID = "org.epic.perleditor";
 	 */
 	public void configure() throws CoreException {
 		// register Perl builder
-		IProjectDescription description = getProject().getDescription();
-		ICommand command = description.newCommand();
-		command.setBuilderName(PLUGIN_ID + ".perlbuilder");
-		System.out.println("Builder name: " + PLUGIN_ID+ ".perlbuilder");
+		//		IProjectDescription description = getProject().getDescription();
+		//		ICommand command = description.newCommand();
+		//		command.setBuilderName(PLUGIN_ID + ".perlbuilder");
+		//		System.out.println("Builder name: " + PLUGIN_ID+ ".perlbuilder");
+		String BUILDER_ID = PLUGIN_ID + ".perlbuilder";
+		IProjectDescription desc = project.getDescription();
+		ICommand[] commands = desc.getBuildSpec();
+		boolean found = false;
+
+		for (int i = 0; i < commands.length; ++i) {
+			if (commands[i].getBuilderName().equals(BUILDER_ID)) {
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			// TODO remove debug output
+			System.out.println("Builder added: " + PLUGIN_ID+ ".perlbuilder");
+			//add builder to project
+			ICommand command = desc.newCommand();
+			command.setBuilderName(BUILDER_ID);
+			ICommand[] newCommands = new ICommand[commands.length + 1];
+
+			// Add it before other builders.
+			System.arraycopy(commands, 0, newCommands, 1, commands.length);
+			newCommands[0] = command;
+			desc.setBuildSpec(newCommands);
+			project.setDescription(desc, null);
+		}
 	}
 
 	/* (non-Javadoc)

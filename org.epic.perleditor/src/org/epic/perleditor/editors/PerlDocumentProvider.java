8bcb8ee42c6e7833be4cc0638471e620b7e29e1d
luelljoc <>
2004-02-08 17:46:48 +0000

    Error correction

--- org.epic.perleditor/src/org/epic/perleditor/editors/PerlDocumentProvider.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/PerlDocumentProvider.java
@@ -23,49 +23,54 @@ import cbg.editor.ColoringDocumentProvider;
 //public class PerlDocumentProvider extends FileDocumentProvider {
 public class PerlDocumentProvider extends ColoringDocumentProvider {
 
-//	private final static String[] TYPES= new String[] {PerlPartitionScanner.PERL_MULTI_LINE_COMMENT, PerlPartitionScanner.PERL_POD_COMMENT };
+	//	private final static String[] TYPES= new String[] {PerlPartitionScanner.PERL_MULTI_LINE_COMMENT, PerlPartitionScanner.PERL_POD_COMMENT };
 
-	private static PerlPartitionScanner fgScanner= null;
+	private static PerlPartitionScanner fgScanner = null;
 
 	public PerlDocumentProvider() {
 		super();
 	}
-	
+
 	/* (non-Javadoc)
 	 * Method declared on AbstractDocumentProvider
 	 */
-	 protected IDocument createDocument(Object element) throws CoreException {
-		IDocument document= super.createDocument(element);
-//		if (document != null) {
-//			IDocumentPartitioner partitioner= createPerlPartitioner();
-//			document.setDocumentPartitioner(partitioner);
-//			partitioner.connect(document);
-//		}
+	protected IDocument createDocument(Object element) throws CoreException {
+		IDocument document = super.createDocument(element);
+		//		if (document != null) {
+		//			IDocumentPartitioner partitioner= createPerlPartitioner();
+		//			document.setDocumentPartitioner(partitioner);
+		//			partitioner.connect(document);
+		//		}
 		return document;
 	}
-	
+
 	/**
 	 * Return a partitioner for .pl files.
 	 */
-//	 private IDocumentPartitioner createPerlPartitioner() {
-//		return new DefaultPartitioner(getPerlPartitionScanner(), TYPES);
-//	}
-	
+	//	 private IDocumentPartitioner createPerlPartitioner() {
+	//		return new DefaultPartitioner(getPerlPartitionScanner(), TYPES);
+	//	}
+
 	/**
 	 * Return a scanner for creating perl partitions.
 	 */
-//	 private PerlPartitionScanner getPerlPartitionScanner() {
-//		if (fgScanner == null)
-//			fgScanner= new PerlPartitionScanner();
-//		return fgScanner;
-//	}
+	//	 private PerlPartitionScanner getPerlPartitionScanner() {
+	//		if (fgScanner == null)
+	//			fgScanner= new PerlPartitionScanner();
+	//		return fgScanner;
+	//	}
 
 	/**
 		 * Replaces createAnnotionModel of the super class.
 		 */
-		protected IAnnotationModel createAnnotationModel(Object element) throws CoreException {
-			IFileEditorInput input= (IFileEditorInput) element;
+	protected IAnnotationModel createAnnotationModel(Object element)
+		throws CoreException {
+		if (element instanceof IFileEditorInput) {
+			IFileEditorInput input = (IFileEditorInput) element;
 			return new PerlSourceAnnotationModel(input);
+		} else {
+			return super.createAnnotationModel(element);
 		}
+	}
 
 }

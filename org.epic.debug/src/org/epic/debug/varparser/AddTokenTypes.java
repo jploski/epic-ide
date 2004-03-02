19fac23e9f5aa7c6c782c51157e18aa1eb6f2994
stephan_ruehl <>
2004-03-02 16:30:34 +0000

    *** empty log message ***

---
+++ org.epic.debug/src/org/epic/debug/varparser/AddTokenTypes.java
@@ -0,0 +1,36 @@
+// $ANTLR : "add.g" -> "PerlParserSimple.java"$
+ package org.epic.debug.varparser; 
+public interface AddTokenTypes {
+	int EOF = 1;
+	int NULL_TREE_LOOKAHEAD = 3;
+	int ARRAY_NAME = 4;
+	int SCALAR_NAME = 5;
+	int HASH_NAME = 6;
+	int MODULE_NAME = 7;
+	int ARRAY_REF = 8;
+	int SCALAR_REF = 9;
+	int HASH_REF = 10;
+	int CODE_REF = 11;
+	int REF = 12;
+	int NUMBER = 13;
+	int SEPARATOR = 14;
+	int INDENT_START = 15;
+	int INDENT_END = 16;
+	int FILE_HANDLE = 17;
+	int FILE_NO = 18;
+	int NL = 19;
+	int EQ = 20;
+	int PAREN_OP = 21;
+	int PAREN_CL = 22;
+	int ADR = 23;
+	int REF_SYMB = 24;
+	int PURE_NAME = 25;
+	int STRING = 26;
+	int KEY_ASSIGN = 27;
+	int FILE_REF = 28;
+	int PREFIXED_NAME = 29;
+	int PURE_NAME_CHAR = 30;
+	int FIRST_PURE_NAME_CHAR = 31;
+	int WS = 32;
+	int CHAR_ESC = 33;
+}

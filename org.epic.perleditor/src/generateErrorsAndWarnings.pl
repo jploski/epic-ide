fb52446f159748406061ecd320dc8d01ac3ce43c
luelljoc <>
2004-05-07 20:42:29 +0000

    Initial version

---
+++ org.epic.perleditor/src/generateErrorsAndWarnings.pl
@@ -0,0 +1,16 @@
+# Creates the errorsAndWarnings.properties file
+open(DIAG, "perldoc -u perldiag |") || die "$!\n";
+while(<DIAG>) { $diag .= $_; }
+close(DIAG);
+
+while($diag =~ /=item(.*?)\n\n(.*?)\n\n/gs) {
+  $count++;
+  $message = $1;
+  $desc = $2;
+  $message =~ s/^\s//;
+  $message =~ s/C</</g;
+  $desc =~ s/\n/\\n/g;
+  $desc =~ s/\t/ /g;
+  print "E$count=$message\t$desc\n";
+}
+

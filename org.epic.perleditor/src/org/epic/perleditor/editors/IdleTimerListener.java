93012d137188c053ec056ddc81bb3733ff6b8ffb
jploski <>
2005-08-27 12:06:18 +0000

    Added documentation.

--- org.epic.perleditor/src/org/epic/perleditor/editors/IdleTimerListener.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/IdleTimerListener.java
@@ -10,6 +10,14 @@ import org.eclipse.jface.text.source.ISourceViewer;
  * @author luelljoc
  *
  */
-public interface IdleTimerListener {
+public interface IdleTimerListener
+{
+    /**
+     * This notification occurs on the Display thread within a configured
+     * time period after the viewed document has changed, provided that
+     * the viewer is visible at that time. If the document has not changed
+     * or the viewer is not visible, the notification may, but does not have
+     * to occur.
+     */
 	public void onEditorIdle(ISourceViewer viewer);
 }

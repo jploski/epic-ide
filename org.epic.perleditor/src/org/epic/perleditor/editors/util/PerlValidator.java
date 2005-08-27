1e8c1c38dd2fba469e80482a3dd66e23a443ed31
jploski <>
2005-08-27 12:09:00 +0000

    Completely rewritten: changed from static methods to singleton, split up large methods, added documentation, moved errorMessagesHash into an own class, normalization of error messages to avoid confusion caused by CGI::Carp, add markers to the resource to which an error message refers rather than to the validated resource.

--- org.epic.perleditor/src/org/epic/perleditor/editors/util/PerlValidator.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/util/PerlValidator.java
@@ -1,452 +1,179 @@
-/*
- * Created on Jan 3, 2004
- *
- * To change the template for this generated file go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
- */
 package org.epic.perleditor.editors.util;
 
-import gnu.regexp.RE;
-
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Hashtable;
+import java.util.*;
 import java.util.List;
 import java.util.Map;
-import java.util.ResourceBundle;
-import java.util.StringTokenizer;
 
 import org.eclipse.core.resources.IMarker;
 import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.jface.text.Document;
-import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.ui.IEditorDescriptor;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.editors.text.TextEditor;
 import org.epic.core.Constants;
 import org.epic.perleditor.PerlEditorPlugin;
-import org.epic.perleditor.editors.AddEditorMarker;
 
 /**
+ * PerlValidator is a singleton class, use PerlValidator.instance() to obtain
+ * an instance of it. 
+ * 
+ * TODO: document this class; much refactoring still needed
+ * TODO: set markers on resources for which errors are reported, not on resources
+ *       whose validation output the report
+ * 
  * @author luelljoc
- *
- * To change the template for this generated type comment go to
- * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ * @author jploski
  */
-public class PerlValidator {
+public class PerlValidator extends PerlValidatorBase
+{   
+    private static PerlValidator instance;    
+    
+    private PerlValidator()
+    {
+        super(PerlEditorPlugin.getDefault().getLog());
+    }
+    
+    /**
+     * @return the PerlValidator singleton
+     */
+    public synchronized static PerlValidator instance()
+    {
+        if (instance == null) instance = new PerlValidator();
+        return instance;
+    }
+	
+    /**
+     * Validates source code of the specified resource (through "perl -c")
+     * and appropriately updates problem markers on this and possibly also
+     * related resources.
+     * 
+     * @param resource
+     *        resource to validate (source code will be read from disk)
+     * @return true if the resource was validated,
+     *         false if it was not validated because it did not require validation
+     * @exception java.io.IOException
+     *            if the source text of the resource could not be read
+     */
+	public synchronized boolean validate(IResource resource)
+        throws PerlValidatorException, IOException
+    {
+		IEditorDescriptor defaultEditorDescriptor =
+			PerlEditorPlugin
+				.getDefault()
+				.getWorkbench()
+				.getEditorRegistry()
+				.getDefaultEditor(resource.getFullPath().toString());
 
-	private static final String PERL_CMD_EXT = "-c";
-	private static final String PERL_ERROR_INDICATOR = " at - line ";
-	private static int maxErrorsShown = 10;
-	//	private static final String[] WARNING_STRINGS =
-	//		{ "possible", "Useless", "may", "better written as" };
-	private static HashMap errorMessagesHash = null;
+		if (defaultEditorDescriptor == null) return false;
 
-	private static final int BUF_SIZE = 1024;
-	
-	private static void initializeErrorsAndWarnings() {
-				//		Initialize Hash containing possible Warning/Error messages
-				try {
-					 if (errorMessagesHash == null) {
-						 errorMessagesHash = new HashMap();
-	
-						 ResourceBundle errorBundle =
-							 ResourceBundle.getBundle(
-								 "org.epic.perleditor.editors.errorsAndWarnings");
-	
-						 RE re;
-	
-						 // Populate the error messages hash
-						 for (Enumeration enum = errorBundle.getKeys();
-							 enum.hasMoreElements();
-							 ) {
-								 String index = (String) enum.nextElement();
-								 String complete = (String) errorBundle.getObject(index);
-								 int tabIndex = complete.indexOf("\t");
-								 String key = complete.substring(0, tabIndex);
-								 String value = complete.substring(tabIndex + 1);
-		
-								 String convKey = new String(key);
-		
-								 // Substitute "( ) [ ]?|*+\" with .
-								 re = new RE("[\\(\\)\\[\\]\\?\\|\\*\\+\\\\]");
-								 convKey = re.substituteAll(convKey, ".");
-		
-								 //	Substitute "%s %c %d %lx" with .*
-								 re = new RE("%([sdcl][x]{0,1})");
-								 convKey = re.substituteAll(convKey, ".*");
-		
-								 // Substitute %.[0-9]s with .*
-								 re = new RE("%\\.[0-9]s");
-								 convKey = re.substituteAll(convKey, ".*");
-		
-								 errorMessagesHash.put(convKey, value);
-						 }
-					 }
-				}
-				catch(Exception e) {
-					 e.printStackTrace();
-				}
-	}
-
-	
-	public static boolean validate(TextEditor textEditor, ISourceViewer viewer) {
-		IEditorInput input = textEditor.getEditorInput();
-		IResource resource = (IResource) ((IAdaptable) input).getAdapter(IResource.class);
-		return validate(resource);
-	}
-	
-	public static boolean validate(IResource resource) {
-		try {
-			StringReaderThread srt = new StringReaderThread();
-			boolean res = validate(resource, srt);
-			srt.dispose();
-			return res;
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		if (!defaultEditorDescriptor.getId().equals(Constants.PERL_EDITOR_ID)
+			|| resource.getFileExtension().equals(Constants.EMB_PERL_FILE_EXTENSION))
+        {
 			return false;
 		}
-	}
-	
-	public static boolean validate(IResource resource, StringReaderThread srt) {
-		try {
-			//	Check if resource should be validated
-			IEditorDescriptor defaultEditorDescriptor =
-				PerlEditorPlugin
-					.getDefault()
-					.getWorkbench()
-					.getEditorRegistry()
-					.getDefaultEditor(resource.getFullPath().toString());
+        
+        // if workspace is out-of-sync with the file system:
+        if (!resource.exists()) return false; 
 
-			if (defaultEditorDescriptor == null) {
-				return false;
-			}
+		validate(resource, readSourceFile(resource));
+        return true;
+	} 
 
-			if (!defaultEditorDescriptor.getId().equals(Constants.PERL_EDITOR_ID)
-				|| resource.getFileExtension().equals(Constants.EMB_PERL_FILE_EXTENSION)) {
-				return false;
-			}
+    protected void addMarker(IResource resource, Map attributes)
+    {
+        new MarkerUtil(resource).addMarker(attributes, IMarker.PROBLEM);
+    }
+    
+    protected void clearAllUsedMarkers(IResource resource)
+    {
+        new MarkerUtil(resource).clearAllUsedFlags(IMarker.PROBLEM);
+    }
+    
+    protected IResource getErrorResource(ParsedErrorLine line, IResource resource)
+    {
+        IResource ret = super.getErrorResource(line, resource);
+        if (ret != null) return ret;
+        
+        // Figure out the project relative path of the resource which contains
+        // an error:
+        IPath projectRoot = resource.getProject().getLocation();
+        String projectRootPath = projectRoot.toOSString();
+        String errorResourcePath = line.getPath();
+        
+        if (errorResourcePath.startsWith(projectRootPath))
+            errorResourcePath = errorResourcePath.substring(projectRootPath.length());
+        else
+            return resource; // resource with error is outside of the project root?
+            
+        IResource errorResource =
+            resource.getProject().findMember(new Path(errorResourcePath));
 
-			StringBuffer sourceCode = new StringBuffer();
-
-			//	Get the file content
-			char[] buf = new char[BUF_SIZE];
-			File inputFile =
-				new File(resource.getLocation().makeAbsolute().toString());
-			BufferedReader in = new BufferedReader(new FileReader(inputFile));
-
-			int read = 0;
-			while ((read = in.read(buf)) > 0) {
-				sourceCode.append(buf, 0, read);
-			}
-			in.close();
-
-			validate(resource, sourceCode.toString(), srt);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-
-		return true;
-	}
-
-	public static void validate(IResource resource, String sourceCode, StringReaderThread srt) {
-		Process proc = null;
-		Map attributes = new HashMap(11);
-		
-		
-		// Initilize Errors and Warnngs Hash;
-		initializeErrorsAndWarnings();
-
-		try {
-			// Construct command line parameters
-			List cmdList =
-				PerlExecutableUtilities.getPerlExecutableCommandLine(
-					resource.getProject());
-			cmdList.add(PERL_CMD_EXT);
-
-			if (PerlEditorPlugin.getDefault().getWarningsPreference()) {
-				cmdList.add("-w");
-			}
-
-			if (PerlEditorPlugin.getDefault().getTaintPreference()) {
-				cmdList.add("-T");
-			}
-
-			String[] cmdParams =
-				(String[]) cmdList.toArray(new String[cmdList.size()]);
-
-			// Get working directory -- Fixes Bug: 736631
-			String workingDir =
-				resource
-					.getLocation()
-					.makeAbsolute()
-					.removeLastSegments(1)
-					.toString();
-
-			/*
-			 * Due to Java Bug #4763384 sleep for a very small amount of time
-			 * immediately after starting the subprocess
-			*/
-			proc =
-				Runtime.getRuntime().exec(
-					cmdParams,
-					null,
-					new File(workingDir));
-			Thread.sleep(1);
-
-			proc.getInputStream().close();
-			InputStream in = proc.getErrorStream();
-			OutputStream out = proc.getOutputStream();
-			//TODO which charset?
-			Reader inr = new InputStreamReader(in);
-			Writer outw = new OutputStreamWriter(out);
-
-			srt.read(inr);
-
-			try {
-				outw.write(sourceCode);
-				outw.write(0x1a); //this should avoid problem with Win98
-				outw.flush();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			out.close();
-
-			String content = srt.getResult();
-			inr.close();
-			in.close();
-
-			//TODO check if content is empty (indicates error)
-
-			// DEBUG start
-			System.out.println("-----------------------------------------");
-			System.out.println("           OUTPUT");
-			System.out.println("-----------------------------------------");
-			System.out.println(content);
-			System.out.println("-----------------------------------------");
-			// DEBUG END
-
-			String line = null;
-			String perlPrintfErrorMessage = null;
-			List lines = new ArrayList();
-			int index;
-
-			StringTokenizer st = new StringTokenizer(content, "\n");
-
-			int lineCount = 0;
-
-			while (st.hasMoreTokens()) {
-				line = st.nextToken();
-				if (line.indexOf("\r") != -1) {
-					line = line.substring(0, line.indexOf("\r"));
-				}
-
-				lines.add(line);
-				if (++lineCount >= maxErrorsShown) {
-					break;
-				}
-
-			}
-
-			// Mark problem markers as unused
-			MarkerUtil markerUtil = new MarkerUtil(resource);
-			markerUtil.clearAllUsedFlags(IMarker.PROBLEM);
-
-			// Hash for tracking line severity
-			Map lineHash = new Hashtable();
-
-			// Markers have to be added in reverse order
-			// Otherwise lower line number will appear at the end of the list
-			for (int i = lines.size() - 1; i >= 0; i--) {
-				line = (String) lines.get(i);
-				perlPrintfErrorMessage = "";
-
-				// Delete filename from error message
-				StringBuffer lineSb = new StringBuffer(line);
-				line = lineSb.toString();
-
-				if ((index = line.indexOf(PERL_ERROR_INDICATOR)) != -1) {
-
-					// truncatedLIne is the stripped error-line up to the next " " after the line number if present
-					// To avoid cluttering with other "." and "," which might occur in the error message
-					String truncatedLine = line;
-					if (truncatedLine
-						.indexOf(" ", index + PERL_ERROR_INDICATOR.length() + 1)
-						!= -1) {
-						truncatedLine =
-							truncatedLine.substring(
-								0,
-								truncatedLine.indexOf(
-									" ",
-									index + PERL_ERROR_INDICATOR.length() + 1));
-					}
-
-					int endIndex;
-					if ((endIndex = truncatedLine.indexOf(".", index)) == -1) {
-						endIndex = truncatedLine.indexOf(",", index);
-					}
-
-					if (endIndex == -1) {
-						continue;
-					}
-
-					String lineNr =
-						truncatedLine.substring(
-							index + PERL_ERROR_INDICATOR.length(),
-							endIndex);
-
-					// If there is an addition to the error message
-					if (i + 1 < lines.size()) {
-						if (((String) lines.get(i + 1)).startsWith(" ")) {
-							line += " " + (String) lines.get(i + 1);
-						}
-					}
-					
-					// If marker is already present continue with next line
-					int lineToCheck = Integer.parseInt(lineNr);
-					if(markerUtil.isMarkerPresent(IMarker.PROBLEM, lineToCheck, line, true)) {
-						continue;
-					}
-
-					// Check if it's a warning
-					boolean isWarning = false;
-
-					Object[] keys = errorMessagesHash.keySet().toArray();
-
-					for (int entry = 0; entry < keys.length; entry++) {
-						RE re = new RE((String) keys[entry]);
-
-						String errorMsgOnly =
-							index != -1
-								? truncatedLine.substring(0, index)
-								: truncatedLine;
-
-						if (re.getAllMatches(errorMsgOnly).length > 0) {
-							String value =
-								(String) errorMessagesHash.get(keys[entry]);
-
-							// (W, D & S) are warnings
-							if (value.startsWith("(W")
-								|| value.startsWith("(D")
-								|| value.startsWith("(S")) {
-								isWarning = true;
-							}
-
-							perlPrintfErrorMessage = (String) value;
-							break;
-						}
-
-					}
-
-					if (isWarning) {
-						attributes.put(
-							IMarker.SEVERITY,
-							new Integer(IMarker.SEVERITY_WARNING));
-					} else {
-						attributes.put(
-							IMarker.SEVERITY,
-							new Integer(IMarker.SEVERITY_ERROR));
-					}
-
-                    // Add generic Perl error message to marker
-					attributes.put(Constants.MARKER_ATTR_PERL_ERROR_EXPLANATION, perlPrintfErrorMessage);
-
-					attributes.put(IMarker.MESSAGE, line);
-
-					attributes.put(
-						IMarker.LINE_NUMBER,
-						new Integer(Integer.parseInt(lineNr)));
-
-					// Check if a marker with a higher severity already exists
-					boolean doUnderline;
-					Object obj =
-						lineHash.get(new Integer(Integer.parseInt(lineNr)));
-					if (obj == null) {
-						doUnderline = true;
-					} else if (
-						((Integer) obj).intValue() == IMarker.SEVERITY_ERROR) {
-						doUnderline = false;
-					} else {
-						doUnderline = true;
-					}
-
-					if (doUnderline) {
-						lineHash.put(
-							new Integer(Integer.parseInt(lineNr)),
-							isWarning
-								? new Integer(IMarker.SEVERITY_WARNING)
-								: new Integer(IMarker.SEVERITY_ERROR));
-						// Get start and end offset
-						int lineOffset = 0;
-						try {
-							Document document = new Document(sourceCode);
-							lineOffset =
-								document.getLineOffset(
-									Integer.parseInt(lineNr) - 1);
-						} catch (Exception ex) {
-							continue;
-						}
-
-						int endOfLine = sourceCode.indexOf("\n", lineOffset);
-						String markerLine;
-
-						if (endOfLine != -1) {
-							markerLine =
-								sourceCode.substring(lineOffset, endOfLine);
-						} else {
-							markerLine = sourceCode.substring(lineOffset);
-						}
-
-						char[] bytes = markerLine.toCharArray();
-
-						int start = 0;
-						while (start < bytes.length) {
-							if (bytes[start] != '\t' && bytes[start] != ' ') {
-								break;
-							}
-							start++;
-						}
-
-						start += lineOffset;
-
-						int end = start + markerLine.trim().length();
-
-						attributes.put(IMarker.CHAR_START, new Integer(start));
-						attributes.put(IMarker.CHAR_END, new Integer(end));
-					}
-
-					// Add markers
-					markerUtil.addMarker(attributes, IMarker.PROBLEM);
-
-				}
-			}
-			
-			// Remove unused markers
-			markerUtil.removeUnusedMarkers(IMarker.PROBLEM);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
-		}
-
-	}
-	
+        // if the resource referred to by the error message is not found,
+        // we shall still attach the marker to the currently validated resource
+        return errorResource != null ? errorResource : resource;
+    }
+    
+    protected List getPerlCommandLine(IResource resource)
+    {
+        List cmdList = PerlExecutableUtilities.getPerlExecutableCommandLine(
+            resource.getProject());        
+    
+        if (PerlEditorPlugin.getDefault().getWarningsPreference())
+            cmdList.add("-w");
+    
+        if (PerlEditorPlugin.getDefault().getTaintPreference())
+            cmdList.add("-T");
+    
+        return cmdList;
+    }
+    
+    protected File getPerlWorkingDir(IResource resource)
+    {
+        return new File(
+            resource
+                .getLocation()
+                .makeAbsolute()
+                .removeLastSegments(1)
+                .toString());
+    }
+    
+    protected boolean isProblemMarkerPresent(
+        ParsedErrorLine line, IResource resource)
+    {
+        return new MarkerUtil(resource).isMarkerPresent(
+            IMarker.PROBLEM, line.getLineNumber(), line.getMessage(), true);
+    }
+    
+    protected void removeUnusedMarkers(IResource resource)
+    {
+        new MarkerUtil(resource).removeUnusedMarkers(IMarker.PROBLEM);
+    }
+    
+    /**
+     * @return true only if there is no existing marker with SEVERITY_ERROR
+     *         on the given line
+     */
+    protected boolean shouldUnderlineError(IResource resource, int lineNr)
+    {
+        List markers = MarkerUtil.getMarkersForLine(resource, lineNr);
+        
+        for (Iterator i = markers.iterator(); i.hasNext();)
+        {
+            IMarker marker = (IMarker) i.next();
+            try
+            {
+                Integer severity = (Integer) marker.getAttribute(IMarker.SEVERITY);
+                if (severity != null && severity.intValue() == IMarker.SEVERITY_ERROR)
+                    return false;
+            }
+            catch (CoreException e)
+            {
+                // thrown if the marker does not exist
+                // it does not interest us then
+            }
+        }
+        return true;
+    }
 }

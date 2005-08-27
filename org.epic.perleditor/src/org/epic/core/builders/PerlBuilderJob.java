73aeba9542ad4514c12674b7005eef2a0e09e3cd
jploski <>
2005-08-27 12:03:57 +0000

    New class used by PerlBuilder; see documentation inside.

---
+++ org.epic.perleditor/src/org/epic/core/builders/PerlBuilderJob.java
@@ -0,0 +1,139 @@
+package org.epic.core.builders;
+
+import java.util.*;
+
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.*;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.viewers.LabelProviderChangedEvent;
+import org.epic.core.ResourceMessages;
+import org.epic.core.decorators.PerlDecorator;
+import org.epic.perleditor.PerlEditorPlugin;
+import org.epic.perleditor.editors.util.PerlValidator;
+
+/**
+ * This low-priority job is started by PerlBuilder and executes
+ * asynchronously to validate Perl files using PerlValidator.
+ * Note that it can be cancelled and rescheduled by PerlBuilder
+ * if another build request comes in the meantime.
+ * 
+ * @author jploski
+ */
+class PerlBuilderJob extends Job
+{
+    /**
+     * Job family used to locate a running instance of this job.
+     */
+    public static final String JOB_FAMILY = "PerlBuilder.jobFamily";
+
+    /**
+     * A list of IResources representing Perl files that still need
+     * to be validated during this job's run.
+     */
+    private final List dirtyResources;
+    
+    /**
+     * A set of IResources representing Perl files that have been
+     * validated during this job's run as well as their parent folders
+     * (and their ancestors).
+     */
+    private final Set validatedResources;
+
+    /**
+     * @param dirtyResources
+     *        a list of IResource instances representing Perl files
+     *        that should be validated; the validation order is
+     *        determined by the order of elements in this list 
+     * @param validatedResources
+     *        an initial set of IResource instances whose labels
+     *        should be updated after this job is complete;
+     *        further resources are added to this set during
+     *        job's execution
+     */
+    public PerlBuilderJob(List dirtyResources, Set successResources)
+    {
+        super(ResourceMessages.getString("PerlBuilderJob.name"));
+        
+        this.validatedResources = successResources;
+        this.dirtyResources = dirtyResources;
+    }
+    
+    public boolean belongsTo(Object family)
+    {
+        return super.belongsTo(family) || JOB_FAMILY.equals(family);
+    }
+
+    /**
+     * @return the remaining Perl files that still need to be validated
+     */
+    public Collection getDirtyResources()
+    {
+        return Collections.unmodifiableList(dirtyResources);
+    }
+    
+    /**
+     * See constructor's documentation.
+     */
+    public Collection getValidatedResources()
+    {
+        return Collections.unmodifiableSet(validatedResources);
+    }
+    
+    protected IStatus run(IProgressMonitor monitor)
+    {
+        PerlDecorator decorator = PerlDecorator.getPerlDecorator();
+        
+        monitor.beginTask("", dirtyResources.size());
+        for (Iterator i = dirtyResources.iterator(); i.hasNext();)
+        {
+            if (monitor.isCanceled()) break;
+            IResource resource = (IResource) i.next();
+            monitor.subTask(resource.getProjectRelativePath().toString());
+            buildResource(resource);
+            i.remove();
+            monitor.worked(1);
+        }
+        monitor.done();       
+        
+        decorator.fireLabelEvent(new LabelProviderChangedEvent(
+            decorator, validatedResources.toArray()));
+        
+        if (monitor.isCanceled()) return Status.CANCEL_STATUS;
+        else return Status.OK_STATUS;
+    }
+    
+    /**
+     * Attempts to validate a single Perl file, schedules label update
+     * for this file and its ancestor folders if validation was performed.
+     */
+    private void buildResource(IResource resource)
+    {
+        try
+        {
+            if (PerlValidator.instance().validate(resource))
+            {
+                validatedResources.add(resource);
+                markParentFoldersUpdated(resource);
+            }
+        }
+        catch (Exception e)
+        {
+            // TODO: it would be better to add info about the exception
+            // as a special error marker on the resource...
+            PerlEditorPlugin.getDefault().getLog().log(
+                new Status(Status.ERROR,
+                    PerlEditorPlugin.getPluginId(),
+                    10002, // TODO: use some sort of constant
+                    "An unexpected exception occurred while validating " +
+                    resource.getProjectRelativePath(),
+                    e));
+        }
+    }
+    
+    private void markParentFoldersUpdated(IResource resource)
+    {
+        while ((resource = resource.getParent()) != null)
+            if (resource.getType() == IResource.FOLDER)
+                validatedResources.add(resource);
+    }
+}

f7dc4ebf6d37f71c7831e1382be6793f9fab4839
jploski <>
2005-09-09 10:58:58 +0000

    Updated to match the new bracket matching code in PerlEditor.java.

--- org.epic.perleditor/src/org/epic/perleditor/actions/Jump2BracketAction.java
+++ org.epic.perleditor/src/org/epic/perleditor/actions/Jump2BracketAction.java
@@ -1,73 +1,48 @@
-package org.epic.perleditor.actions;
-
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.text.source.ISourceViewer;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.editors.text.TextEditor;
-import org.epic.perleditor.editors.PerlEditor;
-
-public class Jump2BracketAction extends Action implements
-		org.eclipse.ui.IEditorActionDelegate {
-
-	PerlEditor editor = null;
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IEditorActionDelegate#setActiveEditor(org.eclipse.jface.action.IAction, org.eclipse.ui.IEditorPart)
-	 */
-	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
-		if (targetEditor instanceof PerlEditor) {
-			editor = ((PerlEditor) targetEditor);
-		}
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
-	 */
-	public void run() {
-		// If fTextEditor is not set, use the current editor
-	    if(editor == null) {
-	    	TextEditor textEditor = (TextEditor) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
-	    	if(textEditor instanceof PerlEditor) {
-	    		editor = (PerlEditor) textEditor;
-	    	}
-	    }
-		
-		if(editor != null) {
-      ISourceViewer viewer = editor.getViewer();
-
-		  int matchPosition = ((PerlEditor) editor).findNextOccurance();
-		  if (matchPosition >= 0) {
-		    viewer.getTextWidget().setCaretOffset(matchPosition +1);
-		    
-		    int newLine = viewer.getTextWidget().getLineAtOffset(matchPosition +1 );
-		    if (viewer.getBottomIndex() <= newLine) {
-		      viewer.setTopIndex(viewer.getTopIndex() + (newLine - viewer.getBottomIndex()));
-		    } else if (viewer.getTopIndex() > newLine) {		      viewer.setTopIndex(newLine);		    }
-		    ((PerlEditor) editor).newCurosorPos();
-		  }
-		}
-		
-	}
-
-	/* (non-Javadoc)
-	 * @see org.eclipse.ui.IActionDelegate#selectionChanged(org.eclipse.jface.action.IAction, org.eclipse.jface.viewers.ISelection)
-	 */
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.ui.IActionDelegate#run(org.eclipse.jface.action.IAction)
-	 */
-	public void run(IAction action) {
-		run();
-	}
-	
+package org.epic.perleditor.actions;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.epic.perleditor.editors.PerlEditor;
+
+public class Jump2BracketAction extends Action
+    implements org.eclipse.ui.IEditorActionDelegate
+{
+    PerlEditor editor = null;
+
+    public void setActiveEditor(IAction action, IEditorPart targetEditor)
+    {
+        if (targetEditor instanceof PerlEditor)
+        {
+            editor = ((PerlEditor) targetEditor);
+        }
+    }
+
+    public void run()
+    {
+        // If fTextEditor is not set, use the current editor
+        if (editor == null)
+        {
+            TextEditor textEditor = (TextEditor) PlatformUI.getWorkbench()
+                .getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+            if (textEditor instanceof PerlEditor)
+            {
+                editor = (PerlEditor) textEditor;
+            }
+        }
+
+        if (editor != null) ((PerlEditor) editor).jumpToMatchingBracket();
+    }
+
+    public void selectionChanged(IAction action, ISelection selection)
+    {
+    }
+
+    public void run(IAction action)
+    {
+        run();
+    }
 }

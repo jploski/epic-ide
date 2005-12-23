bde155473078b78b13911f8639aee537cc928ba8
jploski <>
2005-12-23 23:28:06 +0000

    Removed dependency on org.epic.ext.cbg.editor. Plugged in PerlReconcilingStrategy and PerlSubHyperLinkDetector.

--- org.epic.perleditor/src/org/epic/perleditor/editors/PerlSourceViewerConfiguration.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/PerlSourceViewerConfiguration.java
@@ -1,34 +1,19 @@
 package org.epic.perleditor.editors;
 
-import org.eclipse.swt.graphics.RGB;
-import org.eclipse.jface.text.DefaultAutoIndentStrategy;
-import org.eclipse.jface.text.IAutoIndentStrategy;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.ITextDoubleClickStrategy;
-import org.eclipse.jface.text.ITextHover;
-import org.eclipse.jface.text.TextAttribute;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.text.*;
 import org.eclipse.jface.text.contentassist.ContentAssistant;
 import org.eclipse.jface.text.contentassist.IContentAssistant;
-import org.eclipse.jface.text.rules.BufferedRuleBasedScanner;
-import org.eclipse.jface.text.rules.Token;
-import org.eclipse.jface.text.source.IAnnotationHover;
-import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
+import org.eclipse.jface.text.presentation.IPresentationReconciler;
+import org.eclipse.jface.text.reconciler.IReconciler;
+import org.eclipse.jface.text.reconciler.MonoReconciler;
+import org.eclipse.jface.text.source.*;
+import org.eclipse.swt.graphics.RGB;
 import org.epic.perleditor.PerlEditorPlugin;
-import org.epic.perleditor.editors.perl.PerlAutoIndentStrategy;
-import org.epic.perleditor.editors.perl.PerlCompletionProcessor;
-import org.epic.perleditor.editors.perl.PerlDoubleClickSelector;
-import org.eclipse.ui.editors.text.TextEditor;
-
-import org.epic.perleditor.editors.util.PerlColorProvider;
+import org.epic.perleditor.editors.perl.*;
 import org.epic.perleditor.editors.util.PreferenceUtil;
-
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.epic.perleditor.preferences.PreferenceConstants;
-
-import cbg.editor.*;
-import cbg.editor.rules.*;
-
-import org.eclipse.jface.util.PropertyChangeEvent;
 
 /**
  * @author luelljoc
@@ -36,156 +21,131 @@ import org.eclipse.jface.util.PropertyChangeEvent;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class PerlSourceViewerConfiguration
+public class PerlSourceViewerConfiguration extends SourceViewerConfiguration
+{
+    private final IPreferenceStore prefs;
+    private final PerlEditor editor;
 
-//extends SourceViewerConfiguration {
-		extends ColoringSourceViewerConfiguration {
-	IPreferenceStore fStore;
+    public PerlSourceViewerConfiguration(
+        IPreferenceStore store, PerlEditor textEditor)
+    {
+        assert store != null;
+        
+        prefs = store;
+        editor = textEditor;
+    }
 
-	TextEditor fTextEditor;
+    public IAnnotationHover getAnnotationHover(ISourceViewer sourceViewer)
+    {
+        if (editor == null) return null;
+        return new PerlAnnotationHover(editor);
+    }
 
-	/**
-	 * Single token scanner.
-	 */
+    public IAutoIndentStrategy getAutoIndentStrategy(
+        ISourceViewer sourceViewer, String contentType)
+    {
+        return (IDocument.DEFAULT_CONTENT_TYPE.equals(contentType)
+            ? new PerlAutoIndentStrategy()
+            : new DefaultAutoIndentStrategy());
+    }
+    
+    public String[] getConfiguredContentTypes(ISourceViewer sourceViewer)
+    {
+        return PartitionTypes.getTypes();
+    }
 
-	static class SingleTokenScanner extends BufferedRuleBasedScanner {
-		public SingleTokenScanner(TextAttribute attribute) {
-			setDefaultReturnToken(new Token(attribute));
-		}
-	}
+    public IContentAssistant getContentAssistant(ISourceViewer sourceViewer)
+    {
+        if (editor == null) return null;
+        
+        ContentAssistant assistant = new ContentAssistant();
 
-	protected void adaptToPreferenceChange(PropertyChangeEvent event) {
-		super.adaptToPreferenceChange(event);
-	}
+        // Enable content assist for all content types
+        String[] contentTypes = this.getConfiguredContentTypes(sourceViewer);
+        for (int i = 0; i < contentTypes.length; i++)
+        {
+            assistant.setContentAssistProcessor(
+                new PerlCompletionProcessor(editor), contentTypes[i]);
+        }
 
-	/**
-	 *  
-	 */
-	public PerlSourceViewerConfiguration(IPreferenceStore store,
-			TextEditor textEditor) {
-		//Changed for version 3.0 of Colorer Plugin
-		//super(new ColorManager(store),
-		// EditorPlugin.getDefault().getEditorTools());
-		super(new ColorManager(store));
+        assistant.enableAutoActivation(true);
+        assistant.enableAutoInsert(true);
+        assistant.setAutoActivationDelay(500);
+        assistant.setProposalPopupOrientation(ContentAssistant.PROPOSAL_OVERLAY);
+        assistant.setContextInformationPopupOrientation(ContentAssistant.CONTEXT_INFO_ABOVE);
+        assistant.setContextInformationPopupBackground(
+            PerlEditorPlugin.getDefault().getColor(new RGB(150, 150, 0)));
+        assistant.setProposalSelectorBackground(
+            PerlEditorPlugin.getDefault().getColor(new RGB(254, 241, 233)));
+        assistant.setInformationControlCreator(getInformationControlCreator(sourceViewer));
 
-		//setMode(Modes.getMode("perl.xml"));
-		/*
-		 * String filename;
-		 *  // If we can't get the filname use default name try { filename =
-		 * textEditor.getEditorInput().getName(); } catch(Exception e) {
-		 * filename = "input.pl"; }
-		 * 
-		 * setFilename(filename);
-		 */
-		fStore = store;
-		fTextEditor = textEditor;
-	}
+        return assistant;
+    }
 
-	private IPreferenceStore getPreferenceStore() {
-		return PerlEditorPlugin.getDefault().getPreferenceStore();
-	}
+    public String[] getDefaultPrefixes(
+        ISourceViewer sourceViewer,
+        String contentType)
+    {
+        return new String[] { "#", "" }; //$NON-NLS-1$
 
-	public IAnnotationHover getAnnotationHover(ISourceViewer sourceViewer) {
-		return new PerlAnnotationHover(fTextEditor);
-	}
+    }
 
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	public IAutoIndentStrategy getAutoIndentStrategy(
-			ISourceViewer sourceViewer, String contentType) {
-		return (IDocument.DEFAULT_CONTENT_TYPE.equals(contentType) ? new PerlAutoIndentStrategy()
-				: new DefaultAutoIndentStrategy());
-	}
+    public ITextDoubleClickStrategy getDoubleClickStrategy(
+        ISourceViewer sourceViewer, String contentType)
+    {
+        return new PerlDoubleClickSelector();
+    }
 
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	/*
-	 * public String[] getConfiguredContentTypes(ISourceViewer sourceViewer) {
-	 * return new String[] { IDocument.DEFAULT_CONTENT_TYPE,
-	 * PerlPartitionScanner.PERL_MULTI_LINE_COMMENT }; }
-	 */
+    public IHyperlinkDetector[] getHyperlinkDetectors(ISourceViewer sourceViewer)
+    {
+        if (editor == null) return null;
+        
+        // Add PerlSubHyperlinkDetector to the list provided by the superclass        
 
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	public IContentAssistant getContentAssistant(ISourceViewer sourceViewer) {
+        IHyperlinkDetector[] superDetectors = super.getHyperlinkDetectors(sourceViewer);
+        if (superDetectors == null) superDetectors = new IHyperlinkDetector[0];
 
-		ContentAssistant assistant = new ContentAssistant();
+        IHyperlinkDetector[] ourDetectors =
+            new IHyperlinkDetector[superDetectors.length + 1];
+        ourDetectors[ourDetectors.length - 1] =
+            new PerlSubHyperlinkDetector((PerlEditor) editor);
 
-		//assistant.setContentAssistProcessor(new
-		// PerlCompletionProcessor(fTextEditor),
-		// IDocument.DEFAULT_CONTENT_TYPE);
-		// Enable content assist for all content types
-		String[] contentTypes = this.getConfiguredContentTypes(sourceViewer);
-		for (int i = 0; i < contentTypes.length; i++) {
-			assistant.setContentAssistProcessor(new PerlCompletionProcessor(
-					fTextEditor), contentTypes[i]);
-		}
+        return ourDetectors;
+    }
 
-		assistant.enableAutoActivation(true);
-		assistant.enableAutoInsert(true);
-		assistant.setAutoActivationDelay(500);
-		assistant
-				.setProposalPopupOrientation(ContentAssistant.PROPOSAL_OVERLAY);
-		assistant
-				.setContextInformationPopupOrientation(ContentAssistant.CONTEXT_INFO_ABOVE);
-		assistant.setContextInformationPopupBackground(PerlColorProvider
-				.getColor(new RGB(150, 150, 0)));
-		assistant.setProposalSelectorBackground(PerlColorProvider
-				.getColor(new RGB(254, 241, 233)));
-		assistant
-				.setInformationControlCreator(getInformationControlCreator(sourceViewer));
+    public String[] getIndentPrefixes(ISourceViewer sourceViewer, String contentType)
+    {
+        return new String[] { PreferenceUtil.getTab(0), "\t" };
+    }
 
-		return assistant;
-	}
+    public int getTabWidth(ISourceViewer sourceViewer)
+    {
+        return prefs.getInt(PreferenceConstants.EDITOR_TAB_WIDTH);
+    }
 
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	/*
-	 * public String getDefaultPrefix(ISourceViewer sourceViewer, String
-	 * contentType) { return (IDocument.DEFAULT_CONTENT_TYPE.equals(contentType) ?
-	 * "//" : null); //$NON-NLS-1$
-	 *  }
-	 */
-	public String[] getDefaultPrefixes(ISourceViewer sourceViewer,
-			String contentType) {
-		return (new String[] { "#", "" }); //$NON-NLS-1$
+    public ITextHover getTextHover(ISourceViewer sourceViewer, String contentType)
+    {
+        if (editor == null) return null;
+        
+        return new PerlTextHover(editor);
+    }
+    
+    public IPresentationReconciler getPresentationReconciler(ISourceViewer sourceViewer)
+    {
+        return new PerlPresentationReconciler(prefs);
+    }
 
-	}
+    public IReconciler getReconciler(ISourceViewer sourceViewer)
+    {
+        if (editor == null) return null;
+        
+        MonoReconciler r = new MonoReconciler(
+            new PerlReconcilingStrategy(sourceViewer, editor),
+            false);
 
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	public ITextDoubleClickStrategy getDoubleClickStrategy(
-			ISourceViewer sourceViewer, String contentType) {
-		return new PerlDoubleClickSelector();
-	}
+        r.setDelay(prefs.getInt(
+            PerlEditorPlugin.SYNTAX_VALIDATION_INTERVAL_PREFERENCE));
 
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	public String[] getIndentPrefixes(ISourceViewer sourceViewer,
-			String contentType) {
-		//return new String[] { "\t", " " }; //$NON-NLS-1$ //$NON-NLS-2$
-		return new String[] { PreferenceUtil.getTab(0), "\t" };
-	}
-
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	public int getTabWidth(ISourceViewer sourceViewer) {
-		//return 4;
-		return fStore.getInt(PreferenceConstants.EDITOR_TAB_WIDTH);
-	}
-
-	/*
-	 * Method declared on SourceViewerConfiguration
-	 */
-	public ITextHover getTextHover(ISourceViewer sourceViewer,
-			String contentType) {
-		return new PerlTextHover(fTextEditor);
-	}
+        return r;
+    }
 }

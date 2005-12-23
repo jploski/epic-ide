90b78e471c4f3a6d0c33d3f61ab4591834ae275b
jploski <>
2005-12-23 23:31:21 +0000

    Updated to cache Color resources and properly dispose them when the plug-in is disposed.

--- org.epic.perleditor/src/org/epic/perleditor/editors/util/PerlColorProvider.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/util/PerlColorProvider.java
@@ -1,18 +1,41 @@
 package org.epic.perleditor.editors.util;
 
+import java.util.*;
+
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 
-public class PerlColorProvider {
+public class PerlColorProvider
+{
+    private final Map colors;
+    
+    public PerlColorProvider()
+    {
+        colors = new HashMap();
+    }
+    
+    public synchronized void dispose()
+    {
+        for (Iterator i = colors.keySet().iterator(); i.hasNext();)
+        {
+            Object key = i.next();
+            ((Color) colors.get(key)).dispose();
+            i.remove();
+        }
+    }
 
 	/**
 	 * Return the Color that is stored in the Color table as rgb.
 	 */
-	public static Color getColor(RGB rgb) {
-			Color color= new Color(Display.getCurrent(), rgb);
-
-		return color;
+	public synchronized Color getColor(RGB rgb)
+    {
+        Color c = (Color) colors.get(rgb);
+        if (c == null)
+        {
+            c = new Color(Display.getCurrent(), rgb);
+            colors.put(rgb, c);
+        }
+        return c;
 	}
-
 }

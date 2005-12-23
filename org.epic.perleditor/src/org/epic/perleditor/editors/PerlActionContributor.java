ccbfba3045296a4ad50e73152b61e9adbaabc879
jploski <>
2005-12-23 23:24:03 +0000

    Removed unreachable code.

--- org.epic.perleditor/src/org/epic/perleditor/editors/PerlActionContributor.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/PerlActionContributor.java
@@ -22,35 +22,17 @@ public class PerlActionContributor extends TextEditorActionContributor
         
         PerlEditor editor = (PerlEditor) part;
         
-        if (editor.isPerlMode())
-        {   
-            setGlobalActionHandler(editor, PerlEditorActionIds.CONTENT_ASSIST);
-            setGlobalActionHandler(editor, PerlEditorActionIds.HTML_EXPORT);
-            setGlobalActionHandler(editor, PerlEditorActionIds.VALIDATE_SYNTAX);
-            setGlobalActionHandler(editor, PerlEditorActionIds.FORMAT_SOURCE);
-            setGlobalActionHandler(editor, PerlEditorActionIds.TOGGLE_COMMENT);
-            setGlobalActionHandler(editor, PerlEditorActionIds.OPEN_SUB);
-            setGlobalActionHandler(editor, PerlEditorActionIds.PERL_DOC);
-            setGlobalActionHandler(editor, PerlEditorActionIds.MATCHING_BRACKET);                           
-        }
-        else
-        {
-            resetGlobalActionHandler(PerlEditorActionIds.CONTENT_ASSIST);
-            resetGlobalActionHandler(PerlEditorActionIds.HTML_EXPORT);
-            resetGlobalActionHandler(PerlEditorActionIds.VALIDATE_SYNTAX);
-            resetGlobalActionHandler(PerlEditorActionIds.FORMAT_SOURCE);
-            resetGlobalActionHandler(PerlEditorActionIds.TOGGLE_COMMENT);
-            resetGlobalActionHandler(PerlEditorActionIds.OPEN_SUB);
-            resetGlobalActionHandler(PerlEditorActionIds.PERL_DOC);
-            resetGlobalActionHandler(PerlEditorActionIds.MATCHING_BRACKET);
-        }
+        setGlobalActionHandler(editor, PerlEditorActionIds.CONTENT_ASSIST);
+        setGlobalActionHandler(editor, PerlEditorActionIds.HTML_EXPORT);
+        setGlobalActionHandler(editor, PerlEditorActionIds.VALIDATE_SYNTAX);
+        setGlobalActionHandler(editor, PerlEditorActionIds.FORMAT_SOURCE);
+        setGlobalActionHandler(editor, PerlEditorActionIds.TOGGLE_COMMENT);
+        setGlobalActionHandler(editor, PerlEditorActionIds.OPEN_SUB);
+        setGlobalActionHandler(editor, PerlEditorActionIds.PERL_DOC);
+        setGlobalActionHandler(editor, PerlEditorActionIds.MATCHING_BRACKET);                           
+
         getActionBars().updateActionBars();
 	}
-    
-    private void resetGlobalActionHandler(String perlActionID)
-    {
-        getActionBars().setGlobalActionHandler(perlActionID, null);
-    }
     
     private void setGlobalActionHandler(ITextEditor perlEditor, String perlActionID)
     {

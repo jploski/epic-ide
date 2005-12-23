3f5eeb9fbc1f9171054bfaec15692daa28f5dfe4
jploski <>
2005-12-23 23:24:49 +0000

    PerlDocumentProvider used to be junk code, from now on it is actually used.

--- org.epic.perleditor/src/org/epic/perleditor/editors/PerlDocumentProvider.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/PerlDocumentProvider.java
@@ -1,76 +1,59 @@
 package org.epic.perleditor.editors;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
-//import org.eclipse.jface.text.IDocumentPartitioner;
-//import org.eclipse.jface.text.rules.DefaultPartitioner;
-//import org.eclipse.ui.editors.text.FileDocumentProvider;
-import org.epic.perleditor.editors.perl.PerlPartitionScanner;
+import org.eclipse.jface.text.IDocumentPartitioner;
+import org.eclipse.jface.text.rules.FastPartitioner;
 import org.eclipse.jface.text.source.IAnnotationModel;
 import org.eclipse.ui.IFileEditorInput;
+import org.eclipse.ui.IStorageEditorInput;
+import org.eclipse.ui.editors.text.FileDocumentProvider;
 
-import cbg.editor.ColoringDocumentProvider;
+public class PerlDocumentProvider extends FileDocumentProvider
+{
+    protected IDocument createDocument(Object element)
+        throws CoreException
+    {
+        IDocument doc = super.createDocument(element);
+        String filename = null;
+        if (element instanceof IStorageEditorInput)
+        {
+            filename = ((IStorageEditorInput) element).getStorage().getName();
+        }
+        initializeDocument(doc, filename);
+        return doc;
+    }
 
-/** 
- * The JavaDocumentProvider provides the IDocuments used by java editors.
- */
+    private void initializeDocument(IDocument document, String filename)
+    {
+        if (document != null)
+        {
+            IDocumentPartitioner partitioner = createPartitioner();
+            document.setDocumentPartitioner(partitioner);
+            partitioner.connect(document);
+        }
+    }
 
-//public class PerlDocumentProvider extends FileDocumentProvider {
-public class PerlDocumentProvider extends ColoringDocumentProvider {
+    private IDocumentPartitioner createPartitioner()
+    {
+        /*
+        PerlPartitionScanner scanner = new PerlPartitionScanner();
+        return new FastPartitioner(scanner, scanner.getContentTypes());
+        */
+        return new PerlPartitioner();
+    }
 
-	//	private final static String[] TYPES= new String[] {PerlPartitionScanner.PERL_MULTI_LINE_COMMENT, PerlPartitionScanner.PERL_POD_COMMENT };
-
-	private static PerlPartitionScanner fgScanner = null;
-
-	public PerlDocumentProvider() {
-		super();
-	}
-
-	/* (non-Javadoc)
-	 * Method declared on AbstractDocumentProvider
-	 */
-	protected IDocument createDocument(Object element) throws CoreException {
-		IDocument document = super.createDocument(element);
-		//		if (document != null) {
-		//			IDocumentPartitioner partitioner= createPerlPartitioner();
-		//			document.setDocumentPartitioner(partitioner);
-		//			partitioner.connect(document);
-		//		}
-		return document;
-	}
-
-	/**
-	 * Return a partitioner for .pl files.
-	 */
-	//	 private IDocumentPartitioner createPerlPartitioner() {
-	//		return new DefaultPartitioner(getPerlPartitionScanner(), TYPES);
-	//	}
-
-	/**
-	 * Return a scanner for creating perl partitions.
-	 */
-	//	 private PerlPartitionScanner getPerlPartitionScanner() {
-	//		if (fgScanner == null)
-	//			fgScanner= new PerlPartitionScanner();
-	//		return fgScanner;
-	//	}
-
-	/**
-		 * Replaces createAnnotionModel of the super class.
-		 */
-	protected IAnnotationModel createAnnotationModel(Object element)
-		throws CoreException {
-		if (element instanceof IFileEditorInput) {
-			IFileEditorInput input = (IFileEditorInput) element;
-			return new PerlSourceAnnotationModel(input);
-		} else {
-			return super.createAnnotationModel(element);
-		}
-	}
-
+    protected IAnnotationModel createAnnotationModel(Object element)
+        throws CoreException
+    {
+        if (element instanceof IFileEditorInput)
+        {
+            IFileEditorInput input = (IFileEditorInput) element;
+            return new PerlSourceAnnotationModel(input);
+        }
+        else
+        {
+            return super.createAnnotationModel(element);
+        }
+    }
 }

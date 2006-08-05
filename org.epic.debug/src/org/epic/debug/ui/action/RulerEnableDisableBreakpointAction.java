89b1d425b0965072dc796e3547856dfee481d5d5
jploski <>
2006-08-05 20:28:19 +0000

    Removed an unnecessary import which prevented compilation in 3.1.

--- org.epic.debug/src/org/epic/debug/ui/action/RulerEnableDisableBreakpointAction.java
+++ org.epic.debug/src/org/epic/debug/ui/action/RulerEnableDisableBreakpointAction.java
@@ -4,8 +4,6 @@ import org.eclipse.core.runtime.CoreException;
 
 import org.eclipse.debug.core.model.IBreakpoint;
 import org.eclipse.debug.internal.ui.DebugUIPlugin;
-import org.eclipse.debug.ui.actions.RulerBreakpointAction;
-
 import org.eclipse.jface.text.source.IVerticalRulerInfo;
 
 import org.eclipse.ui.texteditor.ITextEditor;

eaa63e52564a687ca751f198589bf46b006eb006
jgangemi <>
2006-10-31 20:18:41 +0000

    - re-worked work with any marker type in an OO manner

--- org.epic.perleditor/src/org/epic/perleditor/actions/ClearMarkerAction.java
+++ org.epic.perleditor/src/org/epic/perleditor/actions/ClearMarkerAction.java
@@ -1,55 +1,122 @@
 package org.epic.perleditor.actions;
 
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.runtime.IAdaptable;
-import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
+
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IEditorActionDelegate;
-import org.eclipse.ui.IEditorPart;
+
 import org.epic.core.util.MarkerUtilities;
-import org.epic.perleditor.PerlEditorPlugin;
+
 import org.epic.perleditor.editors.PerlEditor;
+import org.epic.perleditor.editors.PerlEditorActionIds;
 
-public class ClearMarkerAction implements IEditorActionDelegate
+
+/**
+ */
+public abstract class ClearMarkerAction extends PerlEditorAction
 {
-    private PerlEditor editor;
+    //~ Constructors
 
-    public void setActiveEditor(IAction action, IEditorPart targetEditor)
+    protected ClearMarkerAction(PerlEditor editor)
     {
-        editor = (PerlEditor) targetEditor;
+        super(editor);
     }
 
-    public void run(IAction action)
-    {
-        // XXX: works ok for specific markers, but needs refactoring to work as a "clear all"
-        MarkerUtilities factory = new MarkerUtilities(getLog(), getPluginId());
-
-        System.out.println("action id" + action.getActionDefinitionId());
-        factory.deleteProblemMarkers(getResource(), action.getActionDefinitionId());
-    }
+    //~ Methods
 
     public void selectionChanged(IAction action, ISelection selection)
     {
         // empty impl
     }
 
-    protected ILog getLog()
+    protected abstract String getMarkerType();
+
+    protected final void doRun()
     {
-        return PerlEditorPlugin.getDefault().getLog();
+        scheduleJob(getMarkerType());
     }
 
-    protected String getPluginId()
+    private void scheduleJob(final String markerType)
     {
-        return PerlEditorPlugin.getPluginId();
+        Job job =
+            new Job("Clear EPIC Marker(s)")
+        {
+            protected IStatus run(IProgressMonitor monitor)
+            {
+                MarkerUtilities factory = new MarkerUtilities(getLog(), getPluginId());
+                factory.deleteMarkers(getResource(), markerType);
+
+                return Status.OK_STATUS;
+            }
+        };
+
+        job.setSystem(true);
+        job.schedule();
+    }
+
+    //~ Inner Classes
+
+    /**
+     */
+    public static class AllMarkers extends ClearMarkerAction
+    {
+
+        public AllMarkers(PerlEditor editor)
+        {
+            super(editor);
+        }
+
+        protected String getMarkerType()
+        {
+            return "org.epic.perleditor.markers.epic";
+        }
+
+        protected String getPerlEditorActionId()
+        {
+            return PerlEditorActionIds.CLEAR_ALL_MARKERS;
+        }
     }
 
     /**
-     * @return returns the resource on which to create the marker, or <code>null</code> if there is
-     *         no applicable resource.
      */
-    protected IResource getResource()
+    public static class Critic extends ClearMarkerAction
     {
-        return (IResource) ((IAdaptable) editor.getEditorInput()).getAdapter(IResource.class);
+        public Critic(PerlEditor editor)
+        {
+            super(editor);
+        }
+
+        protected String getMarkerType()
+        {
+            return "org.epic.perleditor.markers.critic";
+        }
+
+        protected String getPerlEditorActionId()
+        {
+            return PerlEditorActionIds.CLEAR_CRITIC_MARKERS;
+        }
+    }
+
+    /**
+     */
+    public static class PodChecker extends ClearMarkerAction
+    {
+        public PodChecker(PerlEditor editor)
+        {
+            super(editor);
+        }
+
+        protected String getMarkerType()
+        {
+            return "org.epic.perleditor.markers.podChecker";
+        }
+
+        protected String getPerlEditorActionId()
+        {
+            return PerlEditorActionIds.CLEAR_POD_MARKERS;
+        }
     }
 }

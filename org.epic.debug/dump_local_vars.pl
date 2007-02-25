2b0aa9d0d409326732f64f009822e17a9eeb62d8
jploski <>
2007-02-25 20:28:13 +0000

    Reformatting, refactoring and simplification of dumpvar_epic.pm and its clients. Goal: make it work with use strict/warnings, understand the code in order to implement lazy fetching of variable values some day (instead of fetching everything from an entire stack frame, which is slow).

--- org.epic.debug/dump_local_vars.pl
+++ org.epic.debug/dump_local_vars.pl
@@ -1,21 +1,13 @@
 ;{
     if (eval { require PadWalker; PadWalker->VERSION(0.08) })
     {
-        do 'dumpvar_epic.pm' unless defined &dumpvar_epic::dumpvar_epic;
+        do 'dumpvar_epic.pm' unless defined &dumpvar_epic::dump_lexical_vars;
 
-        defined &dumpvar_epic::dumpvar_epic
+        defined &dumpvar_epic::dump_lexical_vars
             or print $DB::OUT "dumpvar_epic.pm not available.\n";
-    
-        my $h = eval { PadWalker::peek_my(2) };
-        my @vars = split(' ', '');
-        $@ and $@ =~ s/ at .*//, print $DB::OUT ($@);
+
         my $savout = select($DB::OUT);
-        dumpvar_epic::dumplex(
-            $_,
-            $h->{$_},
-            defined $option{dumpDepth} ? $option{dumpDepth} : -1,
-            @vars) for sort keys %$h;
-        print "E";
+        dumpvar_epic::dump_lexical_vars();
         select($savout);
     }
     else

af3400af8a038a848bd984fd570ab761324714f3
jploski <>
2007-06-21 16:38:30 +0000

    Added a unit test for bug [ 1741069 ] Array contents not displayed correctly in Variables

--- org.epic.perleditor-test/workspace/EPICTest/test_Variables.pl
+++ org.epic.perleditor-test/workspace/EPICTest/test_Variables.pl
@@ -8,6 +8,12 @@ my @list2 = ( 1, 2, 3 );
 my $arr2 = [ 'aa', 'bb', \@list2 ];
 my $hash = { key1 => $arr2, key2 => \$arr };
 @foo = (1, 2, 3, $hash);
+$foo3 = [1, 2, 3];
+%h = ( key => 'val' );
+$h2 = { key => 'val' };
+$scal = 123;
+$ref1 = \%h;
+$ref2 = \$h2;
 my $foo2 = \@foo;
 
 print "ok\n";

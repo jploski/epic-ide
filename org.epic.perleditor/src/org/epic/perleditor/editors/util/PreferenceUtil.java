ca50af7c29e60eccd56c279e1c3c94beaccd3b59
jploski <>
2007-08-08 10:35:31 +0000

    Minor reformatting.

--- org.epic.perleditor/src/org/epic/perleditor/editors/util/PreferenceUtil.java
+++ org.epic.perleditor/src/org/epic/perleditor/editors/util/PreferenceUtil.java
@@ -3,37 +3,41 @@ package org.epic.perleditor.editors.util;
 import org.epic.perleditor.PerlEditorPlugin;
 import org.epic.perleditor.preferences.PreferenceConstants;
 
-public class PreferenceUtil {
+public class PreferenceUtil
+{
+    /**
+     * @param column
+     * @return
+     */
+    public static String getTab(int column)
+    {
+        boolean useSpaces =
+            PerlEditorPlugin.getDefault().getPreferenceStore()
+            .getBoolean(PreferenceConstants.SPACES_INSTEAD_OF_TABS);
 
-	/**
-	 * @return
-	 */
-	public static String getTab(int column) {
-		boolean useSpaces =
-			PerlEditorPlugin.getDefault().getPreferenceStore().getBoolean(
-				PreferenceConstants.SPACES_INSTEAD_OF_TABS);
+        String tabString = null;
 
+        if (useSpaces)
+        {
+            int numSpaces = PerlEditorPlugin.getDefault().getPreferenceStore()
+                .getInt(PreferenceConstants.INSERT_TABS_ON_INDENT);
 
-		String tabString = null;
-
-		if (useSpaces) {
-			int numSpaces =
-			PerlEditorPlugin.getDefault().getPreferenceStore().getInt(
-				PreferenceConstants.INSERT_TABS_ON_INDENT);
-            
             if (numSpaces > 0)
             {
-			    char[] indentChars = new char[numSpaces - (column % numSpaces)];
-			    for (int i = 0; i < indentChars.length; i++) {
-				    indentChars[i] = ' ';
-			    }
-			    tabString = String.valueOf(indentChars);
+                char[] indentChars = new char[numSpaces - (column % numSpaces)];
+                for (int i = 0; i < indentChars.length; i++)
+                {
+                    indentChars[i] = ' ';
+                }
+                tabString = String.valueOf(indentChars);
             }
             else tabString = "";
-		} else {
-			tabString = "\t";
-		}
+        }
+        else
+        {
+            tabString = "\t";
+        }
 
-		return tabString;
-	}
+        return tabString;
+    }
 }

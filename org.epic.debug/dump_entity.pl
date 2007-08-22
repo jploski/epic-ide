6938fc2e98fb461d964c13645ba4bf2d9b77e779
jploski <>
2007-08-22 21:16:51 +0000

    Fixed bug [ 1779782 ] Variables view very slow showing big arrays.

--- org.epic.debug/dump_entity.pl
+++ org.epic.debug/dump_entity.pl
@@ -5,6 +5,10 @@ do 'dumpvar_epic.pm' unless defined &dumpvar_epic::dump_lexical_vars;
 #SET_VAREXPR#
 #SET_SUBREF#
 my $savout = select($DB::OUT);
+my $savbuf = $|;
+$| = 0;
 $subref->($offset, $varexpr);
+$| = $savbuf;
+print "";
 select($savout);
 };

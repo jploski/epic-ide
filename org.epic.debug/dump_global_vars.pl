794aaf86fe24ff77a62e9989cf99a16fc488c0e7
jploski <>
2008-10-26 13:11:13 +0000

    Implemented feature request [ 2197630 ] Display package-scope ('our') variables in debugger.

--- org.epic.debug/dump_global_vars.pl
+++ org.epic.debug/dump_global_vars.pl
@@ -2,6 +2,6 @@
 do 'dumpvar_epic.pm' unless defined &dumpvar_epic::dump_package_vars;
 
 my $savout = select($DB::OUT);
-dumpvar_epic::dump_package_vars();
+dumpvar_epic::dump_package_vars(__PACKAGE__);
 select($savout);
 };

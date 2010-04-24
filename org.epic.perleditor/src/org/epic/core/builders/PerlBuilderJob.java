66b6a6644ad8b5dd575d0102a41c58341f650ff6
jploski <>
2010-04-24 20:09:18 +0000

    Run Perl::Critic regardless of whether syntax validation succeeds or not.

--- org.epic.perleditor/src/org/epic/core/builders/PerlBuilderJob.java
+++ org.epic.perleditor/src/org/epic/core/builders/PerlBuilderJob.java
@@ -115,15 +115,15 @@ class PerlBuilderJob extends Job
         {
             if (PerlValidator.instance().validate(resource))
             {
-            	// Run Perl::Critic after perl validator has run.
-            	PerlCriticBuilderHelper perlCriticHelper = PerlCriticBuilderHelper.instance();
-            	// check preconditions (perlcritic found and auto-run activated) 
-            	if (perlCriticHelper.isCriticAutoRunPreconditions()) {
-                	perlCriticHelper.buildResource(resource);
-            	}
                 validatedResources.add(resource);
                 markParentFoldersUpdated(resource);
             }
+
+            // Run Perl::Critic after perl validator has run.
+            PerlCriticBuilderHelper perlCriticHelper = PerlCriticBuilderHelper.instance();
+            // check preconditions (perlcritic found and auto-run activated) 
+            if (perlCriticHelper.isCriticAutoRunPreconditions())
+                perlCriticHelper.buildResource(resource);
         }
         catch (CoreException e)
         {

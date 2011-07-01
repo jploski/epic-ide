6858c04458696e6d2c1ee8c5a045b1e0916ed1b9
jploski <jploski@users.sourceforge.net>
2011-07-01 17:30:08 +0000

    Some half-implemented support for breakpoints in ModPerl::RegistryCooker managed scripts

--- org.epic.debug/src/org/epic/debug/remote/RemoteDebugTarget.java
+++ org.epic.debug/src/org/epic/debug/remote/RemoteDebugTarget.java
@@ -1,7 +1,6 @@
 package org.epic.debug.remote;
 
 import java.io.IOException;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.*;
 import org.epic.debug.*;
@@ -64,6 +63,8 @@ class RemoteDebugTarget extends DebugTarget
             }
             catch (IOException e) { throwDebugException(e); }
         }
+        
+        // hackModPerlRegistryCooker(db); // not yet implemented, see method description 
 
         return super.initDebuggerInterface(db);
     }
@@ -71,5 +72,29 @@ class RemoteDebugTarget extends DebugTarget
     private DebuggerProxy getProxy()
     {
         return (DebuggerProxy) getProcess();
+    }
+    
+    /**
+     * Modifies ModPerl::Registry::get_mark_line (called by ModPerl::RegistryCooker)
+     * in order to force the back-end to suspend when the execution of a user script
+     * (via eval) begins.
+     * 
+     * TODO: the corresponding handling in the front-end is not implemented yet. The idea is
+     * that we can get an opportunity to install script-local breakpoints when the artificial
+     * eval-breakpoint created by this method is encountered, in analogy to the installation
+     * of breakpoints upon loading normal Perl modules in epic_breakpoints.pm). 
+     */
+    private void hackModPerlRegistryCooker(DebuggerInterface db) throws DebugException
+    {
+        try
+        {
+            String output = db.eval(
+                ";{if(defined($ENV{MOD_PERL})) { *ModPerl::Registry::get_mark_line = sub { " +
+                "my $self = shift; " +
+                "my $break = \"\\n\\$DB::single = 1;\"; " +
+                "$ModPerl::Registry::MarkLine ? $break . \"\\n#line 1 $self->{FILENAME}\\n\" : $break;" +
+                "}; print $DB::OUT \"mod_perl\\n\"; }}");
+        }
+        catch (IOException e) { throwDebugException(e); }
     }
 }

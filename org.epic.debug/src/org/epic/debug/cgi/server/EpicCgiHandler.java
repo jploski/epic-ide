b2e07ff166b4555991cc051ac7129ec5b19819d8
jploski <jploski@users.sourceforge.net>
2014-11-15 15:03:46 +0100

    Implemented feature request #156 Change Perl CGI file lookup or settings to load the right file

--- org.epic.debug/src/org/epic/debug/cgi/server/EpicCgiHandler.java
+++ org.epic.debug/src/org/epic/debug/cgi/server/EpicCgiHandler.java
@@ -542,7 +542,49 @@ public class EpicCgiHandler implements Handler
         String suffixes,
         String root)
     {
-        // Try to find the shortest prefix in url which can be mapped
+        // Check if the requested URI refers directly to a file or directory.
+        // If it is a file, we got our match - exit early.
+        // If it is a directory, try to locate an index.(suffix) file below it.
+        // Otherwise, proceed with the shortest prefix resolution algorithm.
+        
+        File fileAtURI = new File(root, uri);
+        if (fileAtURI.isFile())
+        {
+            StringTokenizer tok = new StringTokenizer(suffixes, ",");
+            String suffix = null;
+            while (tok.hasMoreTokens())
+            {
+                suffix = tok.nextToken();
+                if (uri.endsWith(suffix))
+                {
+                    request.log(Server.LOG_DIAGNOSTIC, "Found CGI at uri: " + fileAtURI);
+                    return new Object[] { fileAtURI, new Integer(uri.length()) };
+                }
+            }
+            request.log(Server.LOG_DIAGNOSTIC, "Found non-CGI at uri: " + fileAtURI);
+            return null;
+        }
+        else if (fileAtURI.isDirectory())
+        {
+            request.log(Server.LOG_DIAGNOSTIC, "Found directory at uri: " + fileAtURI);
+            
+            StringTokenizer tok = new StringTokenizer(suffixes, ",");
+            String suffix = null;
+            while (tok.hasMoreTokens()) 
+            {
+                suffix = tok.nextToken();
+                File indexFile = new File(root, uri + "index" + suffix);
+                request.log(Server.LOG_DIAGNOSTIC, "Trying " + indexFile);
+                if (indexFile.isFile()) 
+                {
+                    request.log(Server.LOG_DIAGNOSTIC, "Found CGI index at uri: " + indexFile);
+                    return new Object[] { indexFile, new Integer(uri.length()) };
+                }
+            }
+            request.log(Server.LOG_DIAGNOSTIC, "No CGI index found at uri: " + fileAtURI);
+        }
+        
+        // Try to find the shortest prefix in uri which can be mapped
         // to an existing CGI script. This is to correctly extract
         // PATH_INFO from URLs
         // like http://localhost/cgi-bin/foo.cgi/some/path/info
@@ -550,7 +592,7 @@ public class EpicCgiHandler implements Handler
         // (PATH_INFO=/some/path/info)
         
         String suffix = null;
-        StringTokenizer tok = new StringTokenizer(suffixes, ",");
+        StringTokenizer tok = new StringTokenizer(suffixes, ",");        
         int start = 1;
         int end = 0;
 
@@ -561,6 +603,7 @@ public class EpicCgiHandler implements Handler
             start = 1;
             end = 0;
             
+            // Search for the shortest matching prefix
             while (end < uri.length())
             {
                 end = uri.indexOf('/', start);
@@ -570,10 +613,10 @@ public class EpicCgiHandler implements Handler
                 if (!s.endsWith(suffix)) s += suffix;
 
                 File cgiFile = new File(root, s);
-                request.log(Server.LOG_DIAGNOSTIC, "looking for: " + cgiFile);
+                request.log(Server.LOG_DIAGNOSTIC, "Trying " + cgiFile);
                 if (cgiFile.isFile())
                 {
-                    request.log(Server.LOG_DIAGNOSTIC, "found: " + cgiFile);
+                    request.log(Server.LOG_DIAGNOSTIC, "Found CGI at uri: " + cgiFile);
                     return new Object[] { cgiFile, new Integer(end) };
                 }
                 start = end + 1;

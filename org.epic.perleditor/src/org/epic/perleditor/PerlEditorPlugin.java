b50e049be088f3236ee8dac1fe80365928193ca1
V Burns <ancient.wizard@verizon.net>
2018-02-10 12:25:52 -0600

    match original

--- org.epic.perleditor/src/org/epic/perleditor/PerlEditorPlugin.java
+++ org.epic.perleditor/src/org/epic/perleditor/PerlEditorPlugin.java
@@ -13,12 +13,11 @@ import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.team.core.IStringMapping;
+import org.eclipse.team.core.IFileTypeInfo;
 import org.eclipse.team.core.Team;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.eclipse.ui.texteditor.IDocumentProvider;
-import org.epic.core.util.PerlExecutableUtilities;
 import org.epic.core.util.PerlExecutor;
 import org.epic.perleditor.editors.PerlDocumentProvider;
 import org.epic.perleditor.editors.util.PerlColorProvider;
@@ -27,8 +26,6 @@ import org.osgi.framework.BundleContext;
 
 /**
  * The main plugin class to be used in the desktop.
- * 
- * @author jploski?
  */
 public class PerlEditorPlugin extends AbstractUIPlugin {
     //The shared instance.
@@ -54,8 +51,8 @@ public class PerlEditorPlugin extends AbstractUIPlugin {
     /**
      * The constructor.
      */
-    public PerlEditorPlugin()
-    {
+    //public PerlEditorPlugin(IPluginDescriptor descriptor) {
+    public PerlEditorPlugin() {
         //super(descriptor);
         plugin = this;
         try {
@@ -67,7 +64,7 @@ public class PerlEditorPlugin extends AbstractUIPlugin {
 
         // Set team file extensions
         String[] perlTypes = { "pl", "pm" };
-        IStringMapping[] fileTypes = Team.getFileContentManager().getExtensionMappings();
+        IFileTypeInfo[] fileTypes = Team.getAllTypes();
 
         int newTypesLength = fileTypes.length + perlTypes.length;
         String[] extensions = new String[newTypesLength];
@@ -75,7 +72,7 @@ public class PerlEditorPlugin extends AbstractUIPlugin {
 
         int i;
         for (i = 0; i < fileTypes.length; i++) {
-            extensions[i] = fileTypes[i].getString();
+            extensions[i] = fileTypes[i].getExtension();
             types[i] = fileTypes[i].getType();
         }
 
@@ -85,7 +82,7 @@ public class PerlEditorPlugin extends AbstractUIPlugin {
             types[i] = Team.TEXT;
         }
 
-        Team.getFileContentManager().setExtensionMappings(extensions, types);
+        Team.setAllTypes(extensions, types);
     }
 
     /**
@@ -184,18 +181,6 @@ public class PerlEditorPlugin extends AbstractUIPlugin {
         checkForPerlInterpreter(true);
     }
     
-    public String getPerl6Executable()
-    {
-        return getPreferenceStore().getString( PreferenceConstants.DEBUG_PERL6_EXECUTABLE );
-    }
-
-    public void setPerl6Executable( String value )
-    {
-        getPreferenceStore().setValue( PreferenceConstants.DEBUG_PERL6_EXECUTABLE, value );
-        requirePerlErrorDisplayed = false;
-    //  checkForPerlInterpreter( true );
-    }
-    
     public boolean getBooleanPreference(String name) {
         boolean ret = getPreferenceStore().getBoolean(name);
         if (ret) return true;
@@ -266,43 +251,17 @@ public class PerlEditorPlugin extends AbstractUIPlugin {
         final String ERROR_TITLE = "Missing Perl interpreter";
         final String ERROR_MSG =
             "To operate correctly, EPIC requires a Perl interpreter. " +
-            "Check your configuration settings (\"Window -> Preferences -> Perl\").";
+            "Check your configuration settings (\"Window/Preferences/Perl\").";
 
         PerlExecutor executor = new PerlExecutor();
-
         try
         {
             List<String> args = new ArrayList<String>(1);
             args.add("-v");
-
-            /* Test if the configured Perl-5 smells right?
-             *  Run the command, capture stdout, look for expected Perl
-             *  interpreter output.
-             *   $ perl -v
-             *
-             *  TODO: It may make more sense to run a command like the following and avoid
-             *     string inspections altogether. If it runs without error then it's reasonably
-             *     safe to assume it is Perl. You get the version string as a bonus, if you care
-             *    perl -e 'use 5.010001; printf "# %s\n", $]'
-             *   (adjust to your tastes)
-             */
             String stdout = executor.execute(new File("."), args, "").stdout;
-
-            if ( stdout.contains("This is perl 5" ) || stdout.contains("This is cperl 5") > -1 )
+            if (stdout.contains("This is perl") || stdout.contains("This is cperl"))
             {
                 requirePerlCheckPassed = true;
-
-                Status status = new Status(
-                        IStatus.OK,
-                        getPluginId(),
-                        IStatus.OK,
-                        "The PERL-5 executable has been confirmed. " +
-                        PerlExecutableUtilities.getPerlCommandLine() + " -> " + args + "\n" +
-                        "!MESSAGE " + perl_dash_v_output.split( "\n" )[1],
-                        null
-                      );
-
-                getLog().log(status);
             }
             else
             {
@@ -311,11 +270,8 @@ public class PerlEditorPlugin extends AbstractUIPlugin {
                     getPluginId(),
                     IStatus.OK,
                     "The executable specified in Perl Preferences " +
-                    "does not appear to be a valid Perl interpreter. " +
-                    PerlExecutableUtilities.getPerlCommandLine() + " -> " + args + "\n" +
-                    "!MESSAGE " + perl_dash_v_output.split( "\n" )[1],
-                    null
-                  );
+                    "does not appear to be a valid Perl interpreter.",
+                    null);
 
                 getLog().log(status);
                 if (!requirePerlErrorDisplayed || interactive)
